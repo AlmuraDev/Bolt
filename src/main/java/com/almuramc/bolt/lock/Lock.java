bb1314e3f6c7d9e5ee707242a4d3e486e232848c
Chris Sanders <NinjaZidane@hotmail.com>
2012-07-30 19:21:09 -0400

    Headers/Cleanup. Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

--- src/main/java/com/almuramc/bolt/lock/Lock.java
+++ src/main/java/com/almuramc/bolt/lock/Lock.java
@@ -1,19 +1,19 @@
 /*
- * This file is part of AquaLock.
+ * This file is part of Bolt.
  *
  * Copyright (c) 2012, AlmuraDev <http://www.almuramc.com/>
- * AquaLock is licensed under the Almura Development License.
+ * Bolt is licensed under the Almura Development License.
  *
- * AquaLock is free software: you can redistribute it and/or modify
+ * Bolt is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * As an exception, all classes which do not reference GPL licensed code
  * are hereby licensed under the GNU Lesser Public License, as described
- * in Almura Development License.
+ * in the Almura Development License.
  *
- * AquaLock is distributed in the hope that it will be useful,
+ * Bolt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
